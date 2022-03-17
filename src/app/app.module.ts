b26d0a23bc52cc02e972dcdc3db3da022ba4c863
Victor <vctdiniz@gmail.com>
2022-03-17 00:38:43 -0300

    fix modules to use shared/components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { UserModule } from './modules/user/user.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsModule } from 'ngx-bootstrap/tabs'
+import { HomeModule } from './modules/home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     RouterModule,
-    UserModule
+    UserModule,
+    HomeModule,
+    SharedModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
